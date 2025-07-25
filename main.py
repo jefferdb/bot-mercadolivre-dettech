c3dad35005c84ba458ba9b07cf7e89050442e5df
jefferdb <jefferdb93@gmail.com>
2025-07-25 01:34:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Configuração do banco SQLite em memória
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bot_data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////opt/render/project/src/data/bot_data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
